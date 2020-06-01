d89ade4f1fa8369c337b6854616540f31069f508
Sergei Kazhura <kazhura@amazon.com>
2020-06-01 15:55:13 -0400

    sample app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/echo', (req, res) => {
     const message = req.query['message']
