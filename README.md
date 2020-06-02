64c27c3032c904236f7236b6b46e006f003a6701
Sergei Kazhura <kazhura@amazon.com>
2020-06-02 09:45:04 -0400

    stuff

--- README.md
+++ README.md
@@ -13,7 +13,15 @@ sudo yum install git
 git clone https://github.com/serjkazhura/sample-express-api.git
 cd sample-express-api
 
+# in case you want to redirect traffic - but don't do it!
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
 
+# remove the redirect
+sudo iptables -t nat -D PREROUTING --source 0/0 --destination 0/0 -p tcp --dport 80 -j REDIRECT --to-ports 3000
+
 node index.js
+
+# find out the process
+netstat -ltnp | grep -w ':3000'
+kill <PROCESS ID>
 ```
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+const serverName = process.env.SERVER_NAME || 'Node Server';
 
 app.get('/echo', (req, res) => {
     const message = req.query['message']
     console.log(req.query['message'])
-    res.status(200).send({ message: `Your Message is: ${message}`});
+    res.status(200).send({ message: `${serverName} says: ${message}`});
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
